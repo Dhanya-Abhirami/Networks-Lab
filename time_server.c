2c68c3ed8f2389a513a9914df860e555feecfea8
S Dhanya Abhirami <sdhanyaabhirami@gmail.com>
2018-10-14 22:54:32 +0530

    Added Socket Programming Time Server

---
+++ time_server.c
@@ -0,0 +1,45 @@
+#include"netinet/in.h"
+#include"sys/socket.h"
+#include"stdio.h"
+#include"string.h"
+#include"time.h"
+main( )
+{
+	printf("==========TIME SERVER==========\n");
+	printf("\t\t\t\t\tS. DHANYA ABHIRAMI\n");
+	printf("\t\t\t\t\t16BCE0965\n");
+	struct sockaddr_in sa;
+	struct sockaddr_in cli;
+	int sockfd,conntfd;
+	int len,ch;
+	char str[100];
+	time_t tick;
+	sockfd=socket(AF_INET,SOCK_STREAM,0);
+	if(sockfd<0)
+	{
+		printf("error in socket\n");
+		exit(0);
+	}
+	else
+	printf("Socket opened\n");
+	bzero(&sa,sizeof(sa));
+	sa.sin_port=htons(5600);
+	sa.sin_addr.s_addr=htonl(0);
+	if(bind(sockfd,(struct sockaddr*)&sa,sizeof(sa))<0)
+	{
+		printf("Error in binding\n");
+	}
+	else
+	printf("Binded Successfully\n");
+	listen(sockfd,50);
+	for(;;)
+	{
+		len=sizeof(ch);
+		conntfd=accept(sockfd,(struct sockaddr*)&cli,&len);
+		printf("Accepted\n");
+		tick=time(NULL);
+		snprintf(str,sizeof(str),"%s",ctime(&tick));
+		printf("%s",str);
+		write(conntfd,str,100);
+	}
+}
